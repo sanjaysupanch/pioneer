1b30fcc63d81d00910133104f9e29c9ad6601d4c
sanjaysupanch <sanjay.k17@iiits.in>
2019-08-09 14:11:14 +0530

    final

--- pioneer/pioneer/settings.py
+++ pioneer/pioneer/settings.py
@@ -14,7 +14,7 @@ MEDIA_DIR = os.path.join(BASE_DIR,'media')
 SECRET_KEY = 'tb99)qqt5sg4h&#e@q=ru(+vwvsvs(7$mqjez)ma^l*ol0(42i'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
